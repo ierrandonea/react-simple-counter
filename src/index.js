56ed3cca3599b76f05a053dcad692f8762cf7ef9
ierrandonea <ierrandoneag@gmail.com>
2020-08-12 18:07:07 -0400

    updated style, counter and created buttons

--- src/index.js
+++ src/index.js
@@ -7,16 +7,30 @@ import * as serviceWorker from './serviceWorker';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
-const seconds = () => {
-  
-}
+let counter = 0;
+setInterval(() => {
+  let sixDigits = Math.floor(counter/100000);
+  let fiveDigits = Math.floor(counter/10000);
+  let fourDigits = Math.floor(counter/1000);
+  let threeDigits = Math.floor(counter/100);
+  let twoDigits = Math.floor(counter/10);
+  let oneDigit = counter;
+  counter ++;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+    x1={oneDigit}
+    x10={twoDigits}
+    x100={threeDigits}
+    x1000={fourDigits}
+    x10000={fiveDigits}
+    x100000={sixDigits}
+     />
   </React.StrictMode>,
   document.getElementById('root')
 );
+}, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
