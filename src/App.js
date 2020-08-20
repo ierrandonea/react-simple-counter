db604a9e3256230beb42b828cb87636ff5721292
ierrandonea <ierrandoneag@gmail.com>
2020-08-20 11:46:17 -0400

    added missing functionalities

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ function App(props) {
   return (
     <div className="App">
       <div className="Counter">
-            <div className="seconds">
-                <h1 className="clock"><i class="fas fa-stopwatch"></i></h1>
-                <h2 className="10^5">{props.x100000 % 10}</h2>
-                <h2 className="10^4">{props.x10000 % 10}</h2>
-                <h2 className="10^3">{props.x1000 % 10}</h2>
-                <h2 className="10^2">{props.x100 % 10}</h2>
-                <h2 className="10">{props.x10 % 10}</h2>
-                <h2 className="1">{props.x1 % 10}</h2>
-            </div>
-            <button className="buttons">STOP</button>
-            <button className="buttons">RESUME</button>
-            <button className="buttons">RESET</button>
+        <div className="seconds">
+          <h1 className="clock"><i class="fas fa-stopwatch"></i></h1>
+          <h2 className="10^5">{props.x100000 % 10}</h2>
+          <h2 className="10^4">{props.x10000 % 10}</h2>
+          <h2 className="10^3">{props.x1000 % 10}</h2>
+          <h2 className="10^2">{props.x100 % 10}</h2>
+          <h2 className="10">{props.x10 % 10}</h2>
+          <h2 className="1">{props.x1 % 10}</h2>
         </div>
+        <div className="buttons">
+          <button className="buttons">STOP</button>
+          <button className="buttons">RESUME</button>
+          <button className="buttons">RESET</button>
+        </div>
+      </div>
     </div>
   );
 }
